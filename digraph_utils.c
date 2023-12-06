60392fa995d48ba07f98ea8b1ca42371c3d69757
Michael Duda <duda@ucar.edu>
2023-12-05 18:40:35 -0700

    Add additional graph utility routines

--- digraph_utils.c
+++ digraph_utils.c
@@ -2,6 +2,15 @@
 #include <stdlib.h>
 #include "digraph_utils.h"
 
+void init_graph(uint32_t graph[], int n_vertices)
+{
+	int i;
+
+	for (i = 0; i < n_vertices; i++) {
+		graph[i] = 0;
+	}
+}
+
 int read_graph(const char *filename, uint32_t *graph[], int *n_vertices)
 {
 	FILE *f;
@@ -25,4 +34,73 @@ int read_graph(const char *filename, uint32_t *graph[], int *n_vertices)
 	fclose(f);
 
 	return 0;
+}
+
+int write_graph(const char *filename, uint32_t graph[], int n_vertices)
+{
+	FILE *f;
+	int i, j;
+
+	if ((f = fopen(filename, "w")) == NULL) {
+		return 1;
+	}
+
+	fprintf(f, "%d\n", n_vertices);
+	for (i = 0; i < n_vertices; i++) {
+		if (i > 0) fprintf(f, " ");
+		for (j = 24; j >= 0; j -= 8) {
+			fprintf(f, "%02x", (graph[i] >> j) & 0xff);
+		}
+	}
+	fprintf(f, "\n");
+
+	fclose(f);
+
+	return 0;
+}
+
+int add_edge(uint32_t graph[], int n_vertices, int a, int b)
+{
+	if (a < 0 || a >= n_vertices ||
+	    b < 0 || b >= n_vertices) {
+		return 1;	
+	}
+
+	graph[a] |= (1 << b);
+
+	return 0;
+}
+
+int remove_edge(uint32_t graph[], int n_vertices, int a, int b)
+{
+	if (a < 0 || a >= n_vertices ||
+	    b < 0 || b >= n_vertices) {
+		return 1;	
+	}
+
+	graph[a] &= ~(1 << b);
+
+	return 0;
+}
+
+void print_graph(uint32_t graph[], int n_vertices)
+{
+	int i, j;
+
+	printf("digraph {\n");
+	for (i = 0; i < n_vertices; i++) {
+		if (graph[i]) {
+			int need_space = 0;
+			printf("%d -> {", i);
+			for (j = 0; j < n_vertices; j++) {
+				if (graph[i] & (1 << j)) {
+					if (need_space) printf(" ");
+					printf("%d", j);
+					need_space = 1;
+				}
+			}
+			printf("}\n");
+		}
+	}
+	printf("}\n");
 }
